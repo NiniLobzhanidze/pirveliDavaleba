5673ee9e929eb769061aca211c96704f6a6fd197
NiniLobzhanidze <lobzhanidze264@gmail.com>
2024-11-17 12:11:18 +0400

    pirveliDavaleba

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String question = "რა არის შენი პაროლი?";
+   String ipassword = "testing";
+
+   if (ipassword == "testing"){
+       System.out.println("პასუხი სწორია");
+
+   }
+   else {
+       System.out.println("პასუხი არასწორია");
+   }
+
+    }
+}
